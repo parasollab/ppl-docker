e32d1c549fce5282f5ae19483db11005e32fab3b
Ananya Y <14322650+anan-ya-y@users.noreply.github.com>
2025-07-29 13:47:41 -0500

    Update build-ppl-conan.sh with some debug info

--- docker/ppl-dev/scripts/build-ppl-conan.sh
+++ docker/ppl-dev/scripts/build-ppl-conan.sh
@@ -14,7 +14,8 @@ PPLFOLDER=$1
 cd /root/ppl_ws/${PPLFOLDER}
 
 
-conan install . --output-folder=build --profile:build=default -c tools.system.package_manager:mode=install  -c tools.build:jobs=8  -s compiler.cppstd=gnu17 -s build_type=Debug --build missing  -g CMakeDeps  -g CMakeToolchain \
+conan install . --output-folder=build --profile:build=default -c tools.system.package_manager:mode=install  -c tools.build:jobs=8  -s compiler.cppstd=gnu17 -s build_type=Debug --build missing  -g CMakeDeps  -g CMakeToolchain 
+# IF getting some sort of qt build error with cmake_pch.hxx.gch , run with jobs=2 or =1. 
 
 # Change the CMAKE policy version in lib/PQP/CMakeLists.txt (OLD doesn't work with newest CMake...)
 sed -i -e 's/cmake_policy(SET CMP0048 OLD)/cmake_policy(SET CMP0048 NEW)/g' lib/PQP/CMakeLists.txt
