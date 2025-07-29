3c5de1b0d0aa1845cd3a0c9fb2dc532a8bffed9d
Ananya Y <14322650+anan-ya-y@users.noreply.github.com>
2025-07-29 14:39:17 -0500

    Update build-ppl-conan.sh

--- docker/ppl-dev/scripts/build-ppl-conan.sh
+++ docker/ppl-dev/scripts/build-ppl-conan.sh
@@ -15,11 +15,11 @@ cd /root/ppl_ws/${PPLFOLDER}
 
 
 conan install . --output-folder=build --profile:build=default -c tools.system.package_manager:mode=install  -c tools.build:jobs=8  -s compiler.cppstd=gnu17 -s build_type=Debug --build missing  -g CMakeDeps  -g CMakeToolchain 
-# IF getting some sort of qt build error with cmake_pch.hxx.gch , run with jobs=2 or =1. 
+# ^^ IF getting some sort of qt build error with cmake_pch.hxx.gch , run with jobs=2 or =1. 
 
 # Change the CMAKE policy version in lib/PQP/CMakeLists.txt (OLD doesn't work with newest CMake...)
 sed -i -e 's/cmake_policy(SET CMP0048 OLD)/cmake_policy(SET CMP0048 NEW)/g' lib/PQP/CMakeLists.txt
 
 #Build pmpl executable
 cmake -B build -S . -G  Ninja -DCMAKE_TOOLCHAIN_FILE=build/conan_toolchain.cmake -DCMAKE_BUILD_TYPE=Debug -DCODE_COVERAGE=OFF -DBUILD_DOCS=OFF -DCMAKE_POLICY_VERSION_MINIMUM=3.5
-cmake --build build -- -j 4
+cmake --build build -- -j 4 # if getting some sort of error / build not finishing, use -j2 or -j1. 
